85d4b4ae185e84aa2020544fc389e6362652aa5e
mrifkyr95 <57219070+mrifkyr95@users.noreply.github.com>
2019-11-18 23:03:56 +0700

    Add files via upload

---
+++ tugasarrayfix.m
@@ -0,0 +1,8 @@
+HIHI = [8 6 0 1 5 2 ; 2 8 1 0 4 1 ; 4 6 8 1 0 4 ; 9 1 5 1 2 3 ; 9 8 1 5 2 1 ; 9 1 5 2 5 1 ; 8 3 1 6 2 5];
+disp( HIHI(1,3) )
+disp( HIHI(1,4) )
+disp( HIHI(1,1) )
+disp( HIHI(1,5) )
+disp( HIHI(1,4) )
+disp( HIHI(1,5) )
+disp( HIHI(1,4) )
---
+++ tugasarrayfix3.m
@@ -0,0 +1,4 @@
+HUHU = [22 55 ; 33 44 ; 77 66 ; 99 11];
+HEHE = [2 2 ; 3 3 ; 4 4 ; 5 5];
+B = HUHU - HUHU + HEHE + HEHE;
+disp(B);
